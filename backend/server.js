ac5c09158ee39746af54d1bdbe8c270a61e421e0
ETN-Villain <idontdocrypto@outlook.com>
2026-01-28 11:55:44 +0000

    reconcile all games

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,8 @@ app.use("/games", gamesRouter);
 app.use("/events", sseRouter);
 app.use("/nfts", nftsRouter);
 
+await reconcileAllGames();
+
 // ---------------- METADATA ----------------
 app.get("/metadata/:collection/:tokenId", (req, res) => {
   const { collection, tokenId } = req.params;
