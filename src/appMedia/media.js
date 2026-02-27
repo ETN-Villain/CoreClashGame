f3121473a5cd8022c7a7723532b60b294b27ab02
Jordan Butler <jordan.butler2@nhs.net>
2026-02-27 13:34:38 +0000

    jpg to png

--- src/appMedia/media.js
+++ src/appMedia/media.js
@@ -1,6 +1,6 @@
 import CoreClashLogoImg from "./CoreClashLogo.png";
 import PlanetZephyrosAEVideo from "./PlanetZephyrosAE.mp4";
-import ForestOfGlobes from "./ForestOfGlobes.jpg";
+import ForestOfGlobes from "./ForestOfGlobes.png";
 import HowToPlayImg from "./HowToPlay.jpg";
 import GameInfoImg from "./GameInfo.jpg";
 import ElectroSwapImg from "./electroswap.jpg";
