851f081c3318bce85758c35809e1468f54020886
Jordan Butler <jordan.butler2@nhs.net>
2026-04-23 16:26:30 +0100

    try again

--- backend/gameContract.js
+++ backend/gameContract.js
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
-import GameABI from "../../src/abis/GameABI.json" with { type: "json" };
-import { RPC_URL, GAME_ADDRESS, BACKEND_PRIVATE_KEY } from "../config.js";
+import GameABI from "../src/abis/GameABI.json" with { type: "json" };
+import { RPC_URL, GAME_ADDRESS, BACKEND_PRIVATE_KEY } from "./config.js";
 
 export const provider = new ethers.JsonRpcProvider(RPC_URL);
 export const adminWallet = new ethers.Wallet(BACKEND_PRIVATE_KEY, provider);
